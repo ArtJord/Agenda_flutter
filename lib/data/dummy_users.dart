9b62be952cad05c11f2266c9e5ba33801f6a6aff
ArtJord <arthurjordao01@gmail.com>
2024-09-18 20:38:08 -0400

    teste_avatar_null

--- lib/data/dummy_users.dart
+++ lib/data/dummy_users.dart
@@ -2,18 +2,25 @@ import 'package:agenda_flutter/models/user.dart';
 
 const DUMMY_USERS = {
   '1': const User(
-      id: '1', 
-      name: 'Maria',
-       email: 'maria@gmail.com',
-        cell: '5567989787878',
-         avatarUrl:'https://cdn.pixabay.com/photo/2023/03/15/09/32/woman-7854120_1280.png',
-),
-
-'2': const User(
-      id: '2', 
-      name: 'Joao',
-       email: 'joao@gmail.com',
-        cell: '5568900000001',
-         avatarUrl:'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png',
-)
+    id: '1',
+    name: 'Maria',
+    email: 'maria@gmail.com',
+    cell: '5567989787878',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2023/03/15/09/32/woman-7854120_1280.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Joao',
+    email: 'joao@gmail.com',
+    cell: '5568900000001',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Pedro',
+    email: 'pedro@gmail.com',
+    cell: '5567900000002',
+  )
 };
